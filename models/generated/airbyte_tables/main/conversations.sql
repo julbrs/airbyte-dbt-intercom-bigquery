dd4fa55fc594e8d7e437e7c3e5d9afd361fff3fe
Julien Bras <julienbras@sidoine.org>
2021-10-30 09:49:53 -0400

    incremental adjustement

--- models/generated/airbyte_tables/main/conversations.sql
+++ models/generated/airbyte_tables/main/conversations.sql
@@ -1,12 +1,18 @@
-{{ config(schema="main", tags=["top-level"]) }}
+{{ config(
+    schema="main", 
+    tags=["top-level"], 
+    materialized='incremental', 
+    unique_key='_airbyte_hashid', 
+    on_schema_change='append_new_columns',
+    ) }}
 -- Final base SQL model
 select
     'intercom' as source,
     id,
-    created_at,
+    TIMESTAMP_MILLIS(created_at) as created_at,
     conversation_id as ticket_id,
     _airbyte_emitted_at,
-    _airbyte_intercom_conversation_parts_hashid
+    _airbyte_intercom_conversation_parts_hashid as _airbyte_hashid
 from {{ ref('intercom_conversation_parts_ab3') }}
 -- intercom_conversation_parts from {{ source('main', '_airbyte_raw_intercom_conversation_parts') }}
 
--- models/generated/airbyte_tables/main/tickets.sql
+++ models/generated/airbyte_tables/main/tickets.sql
@@ -1,13 +1,19 @@
-{{ config(schema="main", tags=["top-level"]) }}
+{{ config(
+    schema="main", 
+    tags=["top-level"], 
+    materialized='incremental', 
+    unique_key='_airbyte_hashid', 
+    on_schema_change='append_new_columns',
+    ) }}
 -- Final base SQL model
 select
     'intercom' as source,
     id,
-    state,
-    created_at,
-    updated_at,
+    state as status,
+    TIMESTAMP_MILLIS(created_at) as created_at,
+    TIMESTAMP_MILLIS(updated_at) as updated_at,
     _airbyte_emitted_at,
-    _airbyte_intercom_conversations_hashid
+    _airbyte_intercom_conversations_hashid as _airbyte_hashid
 from {{ ref('intercom_conversations_ab3') }}
 -- intercom_conversations from {{ source('main', '_airbyte_raw_intercom_conversations') }}
 
